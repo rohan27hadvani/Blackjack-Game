5636eb131cd0d71dca749a2b7916bf7a383aa790
rohan27hadvani <rohan.hadvani0@gmail.com>
2024-09-06 15:16:36 +0530

    Initial Game

---
+++ index.js
@@ -0,0 +1,65 @@
+
+var cards = []
+var sum = 0
+var hasBlackJack = false
+var isAlive = false
+var message = ""
+
+var messageEl = document.getElementById("message-el")
+var sumEl = document.getElementById("sum-el")
+var cardsEl =document.getElementById("cards-el")
+
+function getRandomCard() {
+    var randomCard = Math.floor(Math.random() * 13) + 1
+    
+
+    if (randomCard > 10) {
+        return 10
+    } 
+    else if (randomCard === 1) {
+        return 11
+    }
+    else {
+        return randomCard;
+    }
+}
+
+function startGame() {
+    isAlive = true;
+    var firstCard = getRandomCard()
+    var secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum 
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } 
+    else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } 
+    else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+   
+}
+
+function newCard() {
+    console.log("Drawing a New Card!")
+    var card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
